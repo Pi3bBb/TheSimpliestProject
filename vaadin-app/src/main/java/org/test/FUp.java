a50691d12f4f189fa7d6e87b7ae815f462db7a38
Pi3bBb <31587008+Pi3bBb@users.noreply.github.com>
2017-09-11 15:14:07 +0300

    Update FUp.java

--- vaadin-app/src/main/java/org/test/FUp.java
+++ vaadin-app/src/main/java/org/test/FUp.java
@@ -2,7 +2,7 @@ package org.test;
 
 public class FUp{
 public static String rename(String name){
-      String result = name + " looser";
+      String result = name + " sucker";
      return result;
 }
 }
